6db9476d7f8f422bb3da77c4785bd0c11c3c3780
TarasFydriaDelphi <Taras.Fydria@udelphi.com>
2024-09-18 21:33:26 +0300

    revove https support

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,10 @@ import errorHandler from "./errorHandler";
 import { cors } from "hono/cors";
 import { config } from "dotenv";
 import { Options } from "@hono/node-server/dist/types";
-import { createServer, ServerOptions } from "node:https";
-import * as fs from "node:fs";
-import * as path from "node:path";
+import { createServer } from "http";
+//import { createServer, ServerOptions } from "node:https";
+//import * as fs from "node:fs";
+//import * as path from "node:path";
 config();
 
 const app = new Hono();
@@ -17,16 +18,16 @@ app.onError(errorHandler);
 
 const port = process.env.HTTP_PORT;
 
-const serverOptions: ServerOptions = {
-  key: fs.readFileSync(path.resolve(__dirname, "..", "localhost-privkey.pem")),
-  cert: fs.readFileSync(path.resolve(__dirname, "..", "localhost-cert.pem")),
-};
+//const serverOptions: ServerOptions = {
+//  key: fs.readFileSync(path.resolve(__dirname, "..", "localhost-privkey.pem")),
+//  cert: fs.readFileSync(path.resolve(__dirname, "..", "localhost-cert.pem")),
+//};
 
 const options: Options = {
   fetch: app.fetch,
   port,
   createServer,
-  serverOptions,
+  //serverOptions,
 };
 
 serve(options);
